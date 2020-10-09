89b277f8028196852ba67825e8ea4373eeabd8ee
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2020-10-09 13:12:45 -0700

    Updated android manifest

--- lib/services/autoBackup.dart
+++ lib/services/autoBackup.dart
@@ -1,6 +1,7 @@
 import 'package:background_fetch/background_fetch.dart';
 import 'package:cron/cron.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:udharokhata/helpers/firebaseBackup.dart';
 
 void autoBackupData(String taskId) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -13,6 +14,7 @@ void autoBackupData(String taskId) async {
   if (todayDate.difference(lastBackupDate).inDays > 7) {
     var cron = new Cron();
     cron.schedule(new Schedule.parse('8-11 * * * *'), () async {
+      FirebaseBackup().backupAllData();
       await prefs.setString('last_backup', todayDate.toString());
     });
   }
